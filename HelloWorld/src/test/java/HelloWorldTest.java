1013cf57cfe4098c1df3effc9dd2275637c98a8f
aristidejou <ajoutane@ttuhsc.edu>
2022-12-19 20:31:48 +0000

    Saved progress on HelloWorld 2022-12-19 20:31:48 +0000

--- HelloWorld/src/test/java/HelloWorldTest.java
+++ HelloWorld/src/test/java/HelloWorldTest.java
@@ -6,9 +6,9 @@ public class HelloWorldTest {
     HelloWorld hw = new HelloWorld();
 
     /**
-     * Method sayHello() must return "Hello, World!". The trim method will remove any extra spaces or newlines at the end.
-     */
-   // @Test
+       * Method sayHello() must return "Hello, World!". The trim method will remove any extra spaces or newlines at the end.
+    */
+    @Test
     public void helloTest(){
         String expected = "Hello, world!";
         String actual = hw.sayHello().trim();
