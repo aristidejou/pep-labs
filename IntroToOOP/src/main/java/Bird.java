eaccec58bfa3f3cd74db3987d50916654bc43574
aristidejou <ajoutane@ttuhsc.edu>
2022-12-20 19:11:41 +0000

    Saved progress on IntroToOOP 2022-12-20 19:11:41 +0000

--- IntroToOOP/src/main/java/Bird.java
+++ IntroToOOP/src/main/java/Bird.java
@@ -45,7 +45,11 @@ public class Bird {
      *  * greater than or equal to 3.
      */
     public String sound(){
-        return "";
+        String s="";
+        if(ageMonths<3) {s="Tweet";}
+        else
+        if(ageMonths>=3){s="Chirp";}
+        return s;
     }
 
 }
