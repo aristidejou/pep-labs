d9ecec67f82881f1c26cfa7d5be05ef0626b5b4f
aristidejou <ajoutane@ttuhsc.edu>
2022-12-21 23:28:24 +0000

    Saved progress on FizzBuzz 2022-12-21 23:28:24 +0000

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -15,6 +15,14 @@ public class FizzBuzz {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
-        return "";
+        String s;
+        if(n%3==0){s="Fizz";}
+        else
+        if(n%5==0){s="Buzz";}
+        else
+        if((n%3==0) && (n%5==0)){s="FizzBuzz";}
+        else s="";
+        
+        return s;
     }
 }
