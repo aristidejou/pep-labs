ea0fcc3bfd75e8ba12675ede067563e022355cba
aristidejou <ajoutane@ttuhsc.edu>
2023-01-05 19:26:50 +0000

    Saved progress on Lists 2023-01-05 19:26:50 +0000

--- Lists/src/main/java/ListExercise.java
+++ Lists/src/main/java/ListExercise.java
@@ -30,8 +30,8 @@ public class ListExercise {
      * @return a List<Integer> object.
      */
     public List<Integer> createList(){
-        List<Integer> list = new ArrayList<>();
-        return list;
+        List<Integer> l = new ArrayList<>();
+        return l;
     }
 
     /**
@@ -53,6 +53,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addToList(List<Integer> list, int value){
+        list.add(value);
     }
 
     /**
@@ -81,7 +82,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeFromList(List<Integer> list, int position){
-
+         list.remove(position);
     }
 
     /**
@@ -94,5 +95,6 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void updateAtPosition(List<Integer> list, int position, int value){
+        list.set(value, position);
     }
 }
