43b55b42c8dcac182b008d5001facabfd1aeedd1
aristidejou <ajoutane@ttuhsc.edu>
2023-01-16 23:14:49 +0000

    Saved progress on ContainsDuplicates 2023-01-16 23:14:49 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -8,6 +8,16 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
-        return false;
+        boolean b=false;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if (nums[i]==nums[j]){
+                  b=true;
+                  j=   nums.length+2;
+                  i=j;     
+                }
+            }
+        }
+        return b;
     }
 }
