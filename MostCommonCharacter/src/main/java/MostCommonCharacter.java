fee6ede8be1a74e778e5ae02a095fd2024631f3d
aristidejou <ajoutane@ttuhsc.edu>
2023-01-20 17:36:17 +0000

    Saved progress on Start 2023-01-20 17:36:17 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -9,28 +9,31 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-       int [] tab={0,0,0,0,0,0,0,0,0,0};
-       int j=0;
-       int max = 0;int k;
-
-        // HashMap<String, Integer> map = new HashMap<>();
-       for(int i=0;i<10;i++){
-        while(j<str.length()){
-            if(str.charAt(j)==i){
-                tab[i]++;
+       // Creating a HashMap containing char
+        // as a key and occurrences as  a value
+        HashMap<Character, Integer> charCountMap
+            = new HashMap<Character, Integer>();
+ 
+        // Converting given string to char array
+ 
+        char[] strArray = str.toCharArray();
+ 
+        // checking each char of strArray
+        for (char c : strArray) {
+            if (charCountMap.containsKey(c)) {
+ 
+                // If char is present in charCountMap,
+                // incrementing it's count by 1
+                charCountMap.put(c, charCountMap.get(c) + 1);
             }
-            j++;
+            else {
+ 
+                // If char is not present in charCountMap,
+                // putting this char to charCountMap with 1 as it's value
+                charCountMap.put(c, 1);
+            }
         }
-        j=0;
-       }
-    
-    for(int i = 1; i < tab.length;i++) { 
-        if(tab[i] > max)
-             {
-        max = tab[i];
-        k=i;
-         }
-        }
-         return k;
+        return 0;
+ 
     }
 }
